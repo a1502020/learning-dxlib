7517c8eef3b9f8ad8ecec42402d21a27d764da7f
Chisaka <a1502020@sendai-nct.jp>
2015-10-27 14:18:29 +0900

    Main を追加

---
+++ STG/STG.cs
@@ -0,0 +1,24 @@
+﻿using DxLibDLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STG
+{
+    class STG
+    {
+        public static void Main(string[] args)
+        {
+            DX.ChangeWindowMode(DX.TRUE);
+            DX.DxLib_Init();
+
+            while (DX.ProcessMessage() == 0)
+            {
+            }
+
+            DX.DxLib_End();
+        }
+    }
+}
