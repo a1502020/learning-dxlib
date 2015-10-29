f48190af1cec534236c8518abc6048ca664b083b
Chisaka <a1502020@sendai-nct.jp>
2015-10-29 23:08:14 +0900

    弾を撃てるようにした
    操作しずらかったのでついでに移動キーを変更

---
+++ STG/Bullet.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STG
+{
+    public class Bullet
+    {
+        /// <summary>
+        /// Bullet を初期化する。
+        /// </summary>
+        /// <param name="position">位置</param>
+        /// <param name="radius">半径</param>
+        /// <param name="angle">進行方向[rad]</param>
+        /// <param name="speed">速度[px/frame]</param>
+        /// <param name="color">色</param>
+        public Bullet(Position position, int radius, double angle, double speed, uint color)
+        {
+            Position = position.Clone();
+            Radius = radius;
+            Angle = angle;
+            Speed = speed;
+            Color = color;
+        }
+
+        /// <summary>
+        /// 位置
+        /// </summary>
+        public Position Position { get; set; }
+
+        /// <summary>
+        /// 半径
+        /// </summary>
+        public int Radius { get; set; }
+
+        /// <summary>
+        /// 進行方向[rad]
+        /// </summary>
+        public double Angle { get; set; }
+
+        /// <summary>
+        /// 速度[px/frame]
+        /// </summary>
+        public double Speed { get; set; }
+
+        /// <summary>
+        /// 色
+        /// </summary>
+        public uint Color { get; set; }
+
+        public Bullet Clone()
+        {
+            return new Bullet(Position, Radius, Angle, Speed, Color);
+        }
+    }
+}
--- STG/STG.cs
+++ STG/STG.cs
@@ -23,6 +23,9 @@ namespace STG
             var ownColor = DX.GetColor(255, 0, 0);
             var ownSpeed = 5.0;
 
+            // 自機の弾
+            var ownBullets = new List<Bullet>();
+
             // キー入力用バッファ
             var keys = new byte[256];
 
@@ -33,19 +36,19 @@ namespace STG
                 DX.GetHitKeyStateAll(out keys[0]);
 
                 // 移動(斜め移動が速くなるのは気にしないことにした)
-                if (keys[DX.KEY_INPUT_W] != 0)
+                if (keys[DX.KEY_INPUT_UP] != 0)
                 {
                     ownPos.Y -= ownSpeed;
                 }
-                if (keys[DX.KEY_INPUT_A] != 0)
+                if (keys[DX.KEY_INPUT_LEFT] != 0)
                 {
                     ownPos.X -= ownSpeed;
                 }
-                if (keys[DX.KEY_INPUT_S] != 0)
+                if (keys[DX.KEY_INPUT_DOWN] != 0)
                 {
                     ownPos.Y += ownSpeed;
                 }
-                if (keys[DX.KEY_INPUT_D] != 0)
+                if (keys[DX.KEY_INPUT_RIGHT] != 0)
                 {
                     ownPos.X += ownSpeed;
                 }
@@ -68,8 +71,29 @@ namespace STG
                     ownPos.Y = 480 - ownRadius;
                 }
 
+                // 弾を発射
+                if (keys[DX.KEY_INPUT_SPACE] != 0)
+                {
+                    ownBullets.Add(new Bullet(ownPos, 5, 3 * Math.PI / 2, 10.0, DX.GetColor(255, 255, 0)));
+                }
+
                 // 画面を黒で塗りつぶし
                 DX.DrawFillBox(0, 0, 640, 480, DX.GetColor(0, 0, 0));
+
+                // 弾の描画と移動
+                foreach (var bullet in ownBullets)
+                {
+                    DX.DrawCircle((int)bullet.Position.X, (int)bullet.Position.Y, bullet.Radius, bullet.Color);
+                    bullet.Position.X += bullet.Speed * Math.Cos(bullet.Angle);
+                    bullet.Position.Y += bullet.Speed * Math.Sin(bullet.Angle);
+                }
+
+                // 画面外の弾を削除
+                ownBullets.RemoveAll(bullet =>
+                    bullet.Position.X < bullet.Radius
+                    || bullet.Position.Y < bullet.Radius
+                    || bullet.Position.X > 640 - bullet.Radius
+                    || bullet.Position.Y > 480 - bullet.Radius);
 
                 // 自機を描画
                 DX.DrawCircle((int)ownPos.X, (int)ownPos.Y, ownRadius, ownColor);
