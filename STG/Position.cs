e87390522b93fa6ced05cfd068f5a3acf3d35ab9
Chisaka <a1502020@sendai-nct.jp>
2015-10-29 22:29:01 +0900

    自機を追加して動かせるようにした

---
+++ STG/Position.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STG
+{
+    /// <summary>
+    /// 2次元平面上の位置を表す。
+    /// </summary>
+    public class Position
+    {
+        /// <summary>
+        /// Position を (x, y) で初期化する。
+        /// </summary>
+        /// <param name="x">X 座標</param>
+        /// <param name="y">Y 座標</param>
+        public Position(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        /// <summary>
+        /// X 座標
+        /// </summary>
+        public double X { get; set; }
+
+        /// <summary>
+        /// Y 座標
+        /// </summary>
+        public double Y { get; set; }
+
+        /// <summary>
+        /// Position を (0, 0) で初期化する。
+        /// </summary>
+        public Position()
+        {
+            X = 0;
+            Y = 0;
+        }
+
+        public Position Clone()
+        {
+            return new Position(X, Y);
+        }
+    }
+}
--- STG/STG.cs
+++ STG/STG.cs
@@ -11,13 +11,73 @@ namespace STG
     {
         public static void Main(string[] args)
         {
+            // 初期化(initialization)
+            DX.SetMainWindowText("Shooting");
             DX.ChangeWindowMode(DX.TRUE);
+            DX.SetDrawScreen(DX.DX_SCREEN_BACK);
             DX.DxLib_Init();
 
+            // 自機
+            var ownPos = new Position(320.0, 240.0);
+            var ownRadius = 16;
+            var ownColor = DX.GetColor(255, 0, 0);
+            var ownSpeed = 5.0;
+
+            // キー入力用バッファ
+            var keys = new byte[256];
+
+            // メインループ
             while (DX.ProcessMessage() == 0)
             {
+                // キー入力情報取得
+                DX.GetHitKeyStateAll(out keys[0]);
+
+                // 移動(斜め移動が速くなるのは気にしないことにした)
+                if (keys[DX.KEY_INPUT_W] != 0)
+                {
+                    ownPos.Y -= ownSpeed;
+                }
+                if (keys[DX.KEY_INPUT_A] != 0)
+                {
+                    ownPos.X -= ownSpeed;
+                }
+                if (keys[DX.KEY_INPUT_S] != 0)
+                {
+                    ownPos.Y += ownSpeed;
+                }
+                if (keys[DX.KEY_INPUT_D] != 0)
+                {
+                    ownPos.X += ownSpeed;
+                }
+
+                // 画面外に行かないようにする
+                if (ownPos.X < ownRadius)
+                {
+                    ownPos.X = ownRadius;
+                }
+                if (ownPos.Y < ownRadius)
+                {
+                    ownPos.Y = ownRadius;
+                }
+                if (ownPos.X > 640 - ownRadius)
+                {
+                    ownPos.X = 640 - ownRadius;
+                }
+                if (ownPos.Y > 480 - ownRadius)
+                {
+                    ownPos.Y = 480 - ownRadius;
+                }
+
+                // 画面を黒で塗りつぶし
+                DX.DrawFillBox(0, 0, 640, 480, DX.GetColor(0, 0, 0));
+
+                // 自機を描画
+                DX.DrawCircle((int)ownPos.X, (int)ownPos.Y, ownRadius, ownColor);
+
+                DX.ScreenFlip();
             }
 
+            // 終了処理(finalization)
             DX.DxLib_End();
         }
     }
